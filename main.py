058d1713b6e9443178ee97ba9f2ee2537d14bc0c
erold90 <61083486+erold90@users.noreply.github.com>
2025-05-31 00:41:00 +0200

    fix: corretto definitivamente errore import handlers

--- main.py
+++ main.py
@@ -51,12 +51,12 @@ from handlers.travel_sheet_handler import (
     register_payment_command
 )
 from handlers.leave_handler import (
-from handlers.rest_handler import rest_command, rest_callback
     leave_command,
     leave_callback,
     add_leave_command,
     plan_leave_command
 )
+from handlers.rest_handler import rest_command, rest_callback
 from handlers.report_handler import (
 from handlers.export_handler import generate_excel_export
     today_command,
