5f41ded661c46c89459a5970802b9c04e612d75a
erold90 <61083486+erold90@users.noreply.github.com>
2025-05-31 00:42:55 +0200

    fix: commentata linea problematica export handler

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ from handlers.leave_handler import (
 )
 from handlers.rest_handler import rest_command, rest_callback
 from handlers.report_handler import (
-# ERRORE: from handlers.export_handler import generate_excel_export
+# from handlers.export_handler import generate_excel_export  # TODO: verificare
     today_command,
     yesterday_command,
     week_command,
