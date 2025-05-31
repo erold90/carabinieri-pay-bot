c63fd75c3335a7177953d498eba2a99bd057e2a6
erold90 <61083486+erold90@users.noreply.github.com>
2025-05-31 14:53:02 +0200

    fix: correzione definitiva blocco try/except in report_handler

--- handlers/report_handler.py
+++ handlers/report_handler.py
@@ -429,13 +429,13 @@ async def export_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     max_length = 0
                     column_letter = column[0].column_letter
                     for cell in column:
+                        try:
                             if len(str(cell.value)) > max_length:
                                 max_length = len(str(cell.value))
                         except:
                             pass
                     adjusted_width = min(max_length + 2, 50)
                     worksheet.column_dimensions[column_letter].width = adjusted_width
-        
         output.seek(0)
         
         # Send file
